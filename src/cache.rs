22f51e2cb82a80d1123895452ed2cecbf0d09b4a
Chawye Hsu <chawyehsu@hotmail.com>
2021-05-05 21:41:58 +0800

    fix: fix cache rm handler
    Signed-off-by: Chawye Hsu <chawyehsu@hotmail.com>

--- src/cache.rs
+++ src/cache.rs
@@ -48,7 +48,7 @@ impl Scoop {
     if cache_dir.exists() {
       match remove_dir_all::remove_dir_contents(cache_dir) {
         Ok(()) => println!("All downloaded caches were removed."),
-        Err(_e) => return Err(anyhow!("Failed to clear the caches"))
+        Err(_e) => return Err(anyhow!("Failed to clear the caches."))
       };
     }
 
@@ -59,26 +59,33 @@ impl Scoop {
     match app_name {
       "*" => self.cache_clean()?,
       _ => {
-        let entries = std::fs::read_dir(&self.cache_dir)?;
+        let entries: Vec<std::fs::DirEntry> = std::fs::read_dir(&self.cache_dir)?
+          .filter_map(Result::ok)
+          .filter(|entry| {
+            let fname = entry.file_name();
+            let fname = fname.to_str().unwrap();
+
+            if app_name.ends_with("*") { // trick to support `scoop cache rm app*`
+              fname.starts_with(&app_name[..app_name.len() - 1])
+            } else {
+              match fname.split_once("#") {
+                Some((a, _b)) => a.eq(app_name),
+                None => false
+              }
+            }
+          })
+          .collect();
+
+        // nothing to do for zero entry
+        if 0 == entries.len() {
+          return Ok(())
+        }
 
         for entry in entries {
-          let entry = entry?;
-          let fname = entry.file_name();
-          let fname = fname.to_str().unwrap();
-          let fname_split: Vec<&str> = fname.split("#").collect();
-
-          // filter files not downloaded by Scoop
-          if 2 > fname_split.len() {
-            continue;
-          }
-
-          if app_name.eq(fname_split[0]) {
-            match std::fs::remove_file(entry.path()) {
-              Ok(()) => println!("All caches of app '{}' was removed.", app_name),
-              Err(_e) => return Err(anyhow!("Failed to remove caches of app '{}'.", app_name))
-            }
-          }
+          std::fs::remove_file(entry.path())?;
         }
+
+        println!("All caches that match '{}' were removed.", app_name);
       }
     }
 
