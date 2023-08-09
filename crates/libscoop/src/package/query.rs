7287bd5672f6eb88ebb52acb928bfbcc6e87877a
Chawye Hsu <su+git@chawyehsu.com>
2023-08-09 17:27:02 +0800

    fix(libscoop|query): don't create empty apps dir
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- crates/libscoop/src/package/query.rs
+++ crates/libscoop/src/package/query.rs
@@ -90,173 +90,186 @@ pub(crate) fn query_installed(
         }
     }
 
-    internal::fs::ensure_dir(&apps_dir)?;
+    let mut ret = vec![];
+    match apps_dir.read_dir() {
+        Err(err) => {
+            debug!("failed to read apps dir ({})", err);
+        }
+        Ok(entries) => {
+            ret = entries
+                .par_bridge()
+                .filter_map(|item| {
+                    if let Ok(e) = item {
+                        let is_dir = e.file_type().map(|t| t.is_dir()).unwrap_or_default();
+                        let filename = e.file_name();
+                        let name = filename.to_str().unwrap();
+                        // The name `scoop` is reserved for Scoop, ignore it
+                        let is_scoop = name == "scoop";
+                        let manifest_path = e.path().join("current/manifest.json");
+                        let install_info_path = e.path().join("current/install.json");
+                        let is_not_broken = manifest_path.exists() && install_info_path.exists();
 
-    let packages = apps_dir
-        .read_dir()?
-        .par_bridge()
-        .filter_map(|item| {
-            if let Ok(e) = item {
-                let is_dir = e.file_type().map(|t| t.is_dir()).unwrap_or_default();
-                let filename = e.file_name();
-                let name = filename.to_str().unwrap();
-                // The name `scoop` is reserved for Scoop, ignore it
-                let is_scoop = name == "scoop";
-                let manifest_path = e.path().join("current/manifest.json");
-                let install_info_path = e.path().join("current/install.json");
-                let is_not_broken = manifest_path.exists() && install_info_path.exists();
-
-                if !is_dir || is_scoop || !is_not_broken {
-                    return None;
-                }
-
-                // Here we can do some pre-filtering by package name, if there
-                // isn't any wildcard query and no extra query requested on
-                // package description or binaries. This could save some query
-                // time by avoiding parsing manifest and install info files.
-                let extra_query = options.contains(&QueryOption::Binary)
-                    || options.contains(&QueryOption::Description);
-                let name_matched = if is_wildcard_query {
-                    // name is always matched for wildcard query
-                    true
-                } else {
-                    matchers.iter().any(|(_, m)| m.is_match(name))
-                };
-
-                if !is_wildcard_query && !extra_query && !name_matched {
-                    return None;
-                }
-
-                if let Ok(manifest) = Manifest::parse(manifest_path) {
-                    if let Ok(install_info) = InstallInfo::parse(install_info_path) {
-                        // Noted that packages installed via URLs don't have
-                        // bucket info in install info file. We mark them as
-                        // isolated packages and use `ISOLATED_PACKAGE_BUCKET`
-                        // as bucket name.
-                        let bucket = install_info.bucket().unwrap_or(ISOLATED_PACKAGE_BUCKET);
-
-                        let mut unmatched = true;
-
-                        if is_wildcard_query {
-                            unmatched = false;
-                        } else {
-                            let prefixed_name_matched = matchers
-                                .iter()
-                                .filter(|&(_, m)| m.is_match(name))
-                                .any(|(prefix, _)| {
-                                    // either no bucket prefix or the bucket
-                                    // is also matched.
-                                    prefix.is_none() || prefix.as_deref().unwrap() == bucket
-                                });
-
-                            if prefixed_name_matched {
-                                unmatched = false;
-                            }
-
-                            if unmatched && !is_explicit_mode {
-                                if options.contains(&QueryOption::Description) {
-                                    let description = manifest.description().unwrap_or_default();
-                                    let description_matched =
-                                        matchers.iter().any(|(_, m)| m.is_match(description));
-                                    if description_matched {
-                                        unmatched = false;
-                                    }
-                                }
-
-                                if options.contains(&QueryOption::Binary) {
-                                    let binaries = manifest.shims().unwrap_or_default();
-                                    let binary_matched = matchers
-                                        .iter()
-                                        .any(|(_, m)| binaries.iter().any(|&b| m.is_match(b)));
-                                    if binary_matched {
-                                        unmatched = false;
-                                    }
-                                }
-                            }
-                        }
-
-                        if unmatched {
+                        if !is_dir || is_scoop || !is_not_broken {
                             return None;
                         }
 
-                        let current_version = manifest.version().to_owned();
+                        // Here we can do some pre-filtering by package name, if there
+                        // isn't any wildcard query and no extra query requested on
+                        // package description or binaries. This could save some query
+                        // time by avoiding parsing manifest and install info files.
+                        let extra_query = options.contains(&QueryOption::Binary)
+                            || options.contains(&QueryOption::Description);
+                        let name_matched = if is_wildcard_query {
+                            // name is always matched for wildcard query
+                            true
+                        } else {
+                            matchers.iter().any(|(_, m)| m.is_match(name))
+                        };
 
-                        let state = InstallState::Installed(InstallStateInstalled {
-                            version: current_version.clone(),
-                            bucket: install_info.bucket().map(|s| s.to_owned()),
-                            arch: install_info.arch().to_owned(),
-                            held: install_info.is_held(),
-                            url: install_info.url().map(|s| s.to_owned()),
-                        });
-
-                        let package = Package::from(name, bucket, manifest);
-                        package.fill_install_state(state.clone());
-
-                        // The query has finished, the package has been found
-                        // and crafted. We can now apply some extra filters.
-                        //
-                        // Filter out packages that are not upgradable when
-                        // the upgradable option is requested.
-                        if options.contains(&QueryOption::Upgradable) {
-                            if bucket == ISOLATED_PACKAGE_BUCKET {
-                                debug!("ignore isolated package '{}'", name);
-                                // isolated packages are not upgradable currently,
-                                // we may support it by live checking the origin
-                                // manifest via the path/url in install_info.
-                                return None;
-                            }
-
-                            let mut bucket_path = root_path.join("buckets");
-                            bucket_path.push(bucket);
-
-                            if let Ok(origin_bucket) = Bucket::from(&bucket_path) {
-                                if let Some(origin_manifest_path) =
-                                    origin_bucket.path_of_manifest(name)
-                                {
-                                    if let Ok(origin_manifest) =
-                                        Manifest::parse(origin_manifest_path)
-                                    {
-                                        let origin_version = origin_manifest.version();
-                                        let is_upgradable =
-                                            compare_versions(origin_version, &current_version)
-                                                == std::cmp::Ordering::Greater;
-                                        if is_upgradable {
-                                            let origin_pkg =
-                                                Package::from(name, bucket, origin_manifest);
-                                            origin_pkg.fill_install_state(state);
-
-                                            package.fill_upgradable(origin_pkg);
-                                        } else {
-                                            // the package is not upgradable,
-                                            // since the upgradable option is
-                                            // requested, we should skip it.
-                                            return None;
-                                        }
-                                    }
-                                } else {
-                                    // the package is not upgradable because
-                                    // the origin manifest is not found. This
-                                    // could happen when the package is deleted
-                                    // or deprecated from the origin bucket.
-                                    return None;
-                                }
-                            } else {
-                                // the package is not upgradable because the
-                                // origin bucket is not reachable. This could
-                                // happen when the bucket is removed or renamed.
-                                return None;
-                            }
+                        if !is_wildcard_query && !extra_query && !name_matched {
+                            return None;
                         }
 
-                        return Some(package);
-                    }
-                }
-            }
-            None
-        })
-        .collect::<Vec<_>>();
+                        if let Ok(manifest) = Manifest::parse(manifest_path) {
+                            if let Ok(install_info) = InstallInfo::parse(install_info_path) {
+                                // Noted that packages installed via URLs don't have
+                                // bucket info in install info file. We mark them as
+                                // isolated packages and use `ISOLATED_PACKAGE_BUCKET`
+                                // as bucket name.
+                                let bucket =
+                                    install_info.bucket().unwrap_or(ISOLATED_PACKAGE_BUCKET);
 
-    Ok(packages)
+                                let mut unmatched = true;
+
+                                if is_wildcard_query {
+                                    unmatched = false;
+                                } else {
+                                    let prefixed_name_matched = matchers
+                                        .iter()
+                                        .filter(|&(_, m)| m.is_match(name))
+                                        .any(|(prefix, _)| {
+                                            // either no bucket prefix or the bucket
+                                            // is also matched.
+                                            prefix.is_none() || prefix.as_deref().unwrap() == bucket
+                                        });
+
+                                    if prefixed_name_matched {
+                                        unmatched = false;
+                                    }
+
+                                    if unmatched && !is_explicit_mode {
+                                        if options.contains(&QueryOption::Description) {
+                                            let description =
+                                                manifest.description().unwrap_or_default();
+                                            let description_matched = matchers
+                                                .iter()
+                                                .any(|(_, m)| m.is_match(description));
+                                            if description_matched {
+                                                unmatched = false;
+                                            }
+                                        }
+
+                                        if options.contains(&QueryOption::Binary) {
+                                            let binaries = manifest.shims().unwrap_or_default();
+                                            let binary_matched = matchers.iter().any(|(_, m)| {
+                                                binaries.iter().any(|&b| m.is_match(b))
+                                            });
+                                            if binary_matched {
+                                                unmatched = false;
+                                            }
+                                        }
+                                    }
+                                }
+
+                                if unmatched {
+                                    return None;
+                                }
+
+                                let current_version = manifest.version().to_owned();
+
+                                let state = InstallState::Installed(InstallStateInstalled {
+                                    version: current_version.clone(),
+                                    bucket: install_info.bucket().map(|s| s.to_owned()),
+                                    arch: install_info.arch().to_owned(),
+                                    held: install_info.is_held(),
+                                    url: install_info.url().map(|s| s.to_owned()),
+                                });
+
+                                let package = Package::from(name, bucket, manifest);
+                                package.fill_install_state(state.clone());
+
+                                // The query has finished, the package has been found
+                                // and crafted. We can now apply some extra filters.
+                                //
+                                // Filter out packages that are not upgradable when
+                                // the upgradable option is requested.
+                                if options.contains(&QueryOption::Upgradable) {
+                                    if bucket == ISOLATED_PACKAGE_BUCKET {
+                                        debug!("ignore isolated package '{}'", name);
+                                        // isolated packages are not upgradable currently,
+                                        // we may support it by live checking the origin
+                                        // manifest via the path/url in install_info.
+                                        return None;
+                                    }
+
+                                    let mut bucket_path = root_path.join("buckets");
+                                    bucket_path.push(bucket);
+
+                                    if let Ok(origin_bucket) = Bucket::from(&bucket_path) {
+                                        if let Some(origin_manifest_path) =
+                                            origin_bucket.path_of_manifest(name)
+                                        {
+                                            if let Ok(origin_manifest) =
+                                                Manifest::parse(origin_manifest_path)
+                                            {
+                                                let origin_version = origin_manifest.version();
+                                                let is_upgradable = compare_versions(
+                                                    origin_version,
+                                                    &current_version,
+                                                )
+                                                    == std::cmp::Ordering::Greater;
+                                                if is_upgradable {
+                                                    let origin_pkg = Package::from(
+                                                        name,
+                                                        bucket,
+                                                        origin_manifest,
+                                                    );
+                                                    origin_pkg.fill_install_state(state);
+
+                                                    package.fill_upgradable(origin_pkg);
+                                                } else {
+                                                    // the package is not upgradable,
+                                                    // since the upgradable option is
+                                                    // requested, we should skip it.
+                                                    return None;
+                                                }
+                                            }
+                                        } else {
+                                            // the package is not upgradable because
+                                            // the origin manifest is not found. This
+                                            // could happen when the package is deleted
+                                            // or deprecated from the origin bucket.
+                                            return None;
+                                        }
+                                    } else {
+                                        // the package is not upgradable because the
+                                        // origin bucket is not reachable. This could
+                                        // happen when the bucket is removed or renamed.
+                                        return None;
+                                    }
+                                }
+
+                                return Some(package);
+                            }
+                        }
+                    }
+                    None
+                })
+                .collect::<Vec<_>>();
+        }
+    }
+
+    Ok(ret)
 }
 
 /// Search available packages.
