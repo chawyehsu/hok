5fd050584e80687452a6dde798824cd312e1b74a
Chawye Hsu <su+git@chawyehsu.com>
2024-12-10 11:06:40 +0800

    feat(hok): add global `--verbose` flag
    and switch to use `tracing_subscriber` for enhanced logging
    
    Signed-off-by: Chawye Hsu <su+git@chawyehsu.com>

--- src/cmd/mod.rs
+++ src/cmd/mod.rs
@@ -1,4 +1,9 @@
 use clap::{crate_description, crate_name, crate_version, Parser, Subcommand};
+use clap_verbosity_flag::Verbosity;
+use libscoop::Session;
+use tracing_subscriber::{
+    filter::LevelFilter, layer::SubscriberExt, util::SubscriberInitExt, EnvFilter,
+};
 
 mod bucket;
 mod cache;
@@ -18,13 +23,16 @@ mod update;
 mod upgrade;
 
 use crate::Result;
-use libscoop::Session;
 
 #[derive(Parser)]
 #[command(
     name = crate_name!(),
     version = crate_version!(),
     about = crate_description!(),
+    long_about = format!("{}
+
+If you find any bugs or have a feature request, please open an issue on
+GitHub: https://github.com/chawyehsu/hok/issues", crate_description!()),
     subcommand_required = true,
     arg_required_else_help = true,
     max_term_width = 100,
@@ -36,6 +44,10 @@ use libscoop::Session;
 pub struct Cli {
     #[command(subcommand)]
     pub command: Command,
+
+    /// The verbosity level
+    #[command(flatten)]
+    verbose: Verbosity,
 }
 
 #[derive(Subcommand)]
@@ -63,25 +75,65 @@ pub enum Command {
 }
 
 /// CLI entry point
-pub fn start(session: &Session) -> Result<()> {
+pub fn start() -> Result<()> {
     let args = Cli::parse();
+    setup_logger(args.verbose.tracing_level_filter())?;
+
+    let session = Session::default();
+    let user_agent = format!("Scoop/1.0 (+https://scoop.sh/) Hok/{}", crate_version!());
+    let _ = session.set_user_agent(&user_agent);
 
     match args.command {
-        Command::Bucket(args) => bucket::execute(args, session),
-        Command::Cache(args) => cache::execute(args, session),
-        Command::Cat(args) => cat::execute(args, session),
-        Command::Cleanup(args) => cleanup::execute(args, session),
+        Command::Bucket(args) => bucket::execute(args, &session),
+        Command::Cache(args) => cache::execute(args, &session),
+        Command::Cat(args) => cat::execute(args, &session),
+        Command::Cleanup(args) => cleanup::execute(args, &session),
         Command::Completions(args) => completions::execute(args),
-        Command::Config(args) => config::execute(args, session),
-        Command::Hold(args) => hold::execute(args, session),
-        Command::Home(args) => home::execute(args, session),
-        Command::Info(args) => info::execute(args, session),
-        Command::Install(args) => install::execute(args, session),
-        Command::List(args) => list::execute(args, session),
-        Command::Search(args) => search::execute(args, session),
-        Command::Unhold(args) => unhold::execute(args, session),
-        Command::Uninstall(args) => uninstall::execute(args, session),
-        Command::Update(args) => update::execute(args, session),
-        Command::Upgrade(args) => upgrade::execute(args, session),
+        Command::Config(args) => config::execute(args, &session),
+        Command::Hold(args) => hold::execute(args, &session),
+        Command::Home(args) => home::execute(args, &session),
+        Command::Info(args) => info::execute(args, &session),
+        Command::Install(args) => install::execute(args, &session),
+        Command::List(args) => list::execute(args, &session),
+        Command::Search(args) => search::execute(args, &session),
+        Command::Unhold(args) => unhold::execute(args, &session),
+        Command::Uninstall(args) => uninstall::execute(args, &session),
+        Command::Update(args) => update::execute(args, &session),
+        Command::Upgrade(args) => upgrade::execute(args, &session),
     }
+}
+
+fn setup_logger(level_filter: LevelFilter) -> Result<()> {
+    // filter for low-level/depedency logs
+    let low_level_filter = match level_filter {
+        LevelFilter::OFF => LevelFilter::OFF,
+        LevelFilter::ERROR => LevelFilter::ERROR,
+        LevelFilter::WARN => LevelFilter::WARN,
+        LevelFilter::INFO => LevelFilter::WARN,
+        LevelFilter::DEBUG => LevelFilter::INFO,
+        LevelFilter::TRACE => LevelFilter::TRACE,
+    };
+
+    let mut layer_env_filter = EnvFilter::builder()
+        .with_default_directive(level_filter.into())
+        .from_env()?;
+
+    // The custom `HOK_LOG_LEVEL` environment variable was introduced to set the
+    // log level for hok since the first version.
+    if let Ok(level) = std::env::var("HOK_LOG_LEVEL") {
+        layer_env_filter = layer_env_filter.add_directive(format!("libscoop={level}").parse()?);
+    }
+
+    layer_env_filter = layer_env_filter
+        // add low-level filter for git2
+        .add_directive(format!("git2={}", low_level_filter).parse()?);
+
+    let layer_fmt = tracing_subscriber::fmt::layer().without_time();
+
+    tracing_subscriber::registry()
+        .with(layer_env_filter)
+        .with(layer_fmt)
+        .init();
+
+    Ok(())
 }
--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,6 @@ use crossterm::{
     style::{Color, Print, SetForegroundColor},
     ExecutableCommand,
 };
-use libscoop::Session;
 use std::{fmt::Display, io};
 
 mod cmd;
@@ -10,16 +9,6 @@ mod cui;
 mod util;
 
 type Result<T> = anyhow::Result<T>;
-
-fn create_logger() -> Result<()> {
-    let env = env_logger::Env::default()
-        .filter_or("HOK_LOG_LEVEL", "error")
-        .write_style("never");
-    match env_logger::try_init_from_env(env) {
-        Ok(_) => Ok(()),
-        Err(e) => Err(e.into()),
-    }
-}
 
 fn error<T: Display>(input: &T) -> io::Result<()> {
     let mut stderr = io::stderr();
@@ -32,8 +21,8 @@ fn error<T: Display>(input: &T) -> io::Result<()> {
     Ok(())
 }
 
-fn report(err: anyhow::Error) {
-    let _ = error(&err);
+fn report(err: &anyhow::Error) {
+    let _ = error(err);
     if let Some(cause) = err.source() {
         eprintln!("\nCaused by:");
         for (i, e) in std::iter::successors(Some(cause), |e| e.source()).enumerate() {
@@ -43,15 +32,5 @@ fn report(err: anyhow::Error) {
 }
 
 pub fn create_app() -> bool {
-    let _ = create_logger();
-    let session = Session::default();
-    let _ = session.set_user_agent("Scoop/1.0 (+http://scoop.sh/) Hok/0.1.0");
-
-    match cmd::start(&session) {
-        Ok(_) => false,
-        Err(e) => {
-            report(e);
-            true
-        }
-    }
+    cmd::start().inspect_err(report).is_err()
 }
